b1bfa11df7087fa03492ebb7b91ce3ea3528cd94
Alexey <alexeybe@checkpoint.com>
2020-12-25 16:41:47 +0200

    Rewriting histroy

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # jenkins_manager
+Class to control several aspects of Jenkins jobs' lifecycle:
+
+## Jobs execution flow:
+```
+. Trigger asynchronously N jobs.
+. Report any failures.
+```
+
+## Job creation and dumping:
+```
+. Dump jobs' configuration
+. Create jobs from dumps.
+```
+
+## Example
+Several use case examples in `tests` dir.
---
+++ src/jenkins_job.py
@@ -0,0 +1,30 @@
+"""
+Module handling jenkins job.
+All steps - from being triggered to the final result.
+"""
+import uuid
+
+
+class JenkinsJob:
+    """
+    Class handling Jenkins job. Has all attributes to manage it's current step.
+    """
+
+    def __init__(self, name, parameters, uid_parameter_name=None):
+        self.name = name
+        self.parameters = parameters
+        self.queue_item_id = None
+        self.build_id = None
+        self.build_status = None
+        self.uid = str(uuid.uuid4())
+        self.uid_parameter_name = uid_parameter_name
+
+    def get_request_parameters(self):
+        """
+        Generate parameters as needed for correct job triggering.
+        :return:
+        """
+        parameters = self.parameters
+        if self.uid_parameter_name is not None:
+            parameters[self.uid_parameter_name] = self.uid
+        return parameters
